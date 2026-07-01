ac1722a82ae768f15a4a4fb3874b58b5dc755ab6
JAjere-lej <jeremypaucar305@gmail.com>
2026-07-01 16:42:29 -0500

    Add C# program with variable examples
    This file contains a C# program demonstrating variable declarations and data types.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/JAjere-lej.cs
@@ -0,0 +1,39 @@
+using System;
+namespace CSharpPractica
+{       
+    class Practica 
+    {
+    static void Main(string[] args)
+        {
+            // https://dotnet.microsoft.com/es-es/languages/csharp
+
+            // curry esta loco
+
+            /* 
+            curry 
+            esta
+            loco
+            */
+
+            var myVariable = "Chef Curry";
+            myVariable = "Splash";
+            
+
+            const string myConst = "Mi constante";
+            
+            int myInt = 30;
+            
+            double myDouble = 30.5;
+
+            float myFloat = 30.5f;
+
+            bool myBool = true;
+            bool myBool = false;
+
+            string myString = "Mi cadena de texto";
+
+            Console.WriteLine("Hola curry");
+            
+        }
+    }
+}
